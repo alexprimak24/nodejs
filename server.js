ace6b7282072e6be432fa0da8bebf2652ff498fe
alexprimak58 <aprimak43@gmail.com>
2024-08-01 15:44:57 +0300

    viewEngine

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const path = require("path");
 
 const app = express();
 
+app.set("view engine", "ejs");
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, "views", `${page}.html`);
+const createPath = (page) =>
+  path.resolve(__dirname, "ejs-views", `${page}.ejs`);
 
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`);
@@ -14,19 +16,43 @@ app.listen(PORT, (error) => {
 
 //thanks to express it automatically sets content type we are sending
 app.get("/", (req, res) => {
-  res.sendFile(createPath("index"));
+  const title = "Home";
+  res.render(createPath("index"), { title });
 });
 
 app.get("/contacts", (req, res) => {
-  res.sendFile(createPath("contacts"));
+  const title = "Contacts";
+  const contacts = [
+    { name: "youtube", link: "https://www.youtube.com" },
+    { name: "youtube", link: "https://www.youtube.com" },
+    { name: "youtube", link: "https://www.youtube.com" },
+  ];
+  res.render(createPath("contacts"), { contacts, title });
 });
 //redirect to an updated route
 app.get("/about-us", (req, res) => {
-  res.redirect(createPath("/contacts"));
+  const title = "Contacts";
+  res.render(createPath("/contacts"), { title });
+});
+
+app.get("/posts/:id", (req, res) => {
+  const title = "Post";
+  res.render(createPath("post"), { title });
+});
+
+app.get("/posts", (req, res) => {
+  const title = "Posts";
+  res.render(createPath("posts"), { title });
+});
+
+app.get("/add-post", (req, res) => {
+  const title = "AddPost";
+  res.render(createPath("add-post"), { title });
 });
 
 //it is a middleware
 app.use((req, res) => {
+  const title = "ErrorPage";
   // res.statusCode = 404; we can just use status
-  res.status(404).sendFile(createPath("error"));
+  res.status(404).render(createPath("error"), { title });
 });
